bcdfbe4e8a3f893ace1a9293f9b517b9a57b2fc7
Keyur jada <159427338+keyurjada@users.noreply.github.com>
2025-11-14 12:01:59 +0530

    first try to solve leetcode problem

---
+++ leetcode.c
@@ -0,0 +1,11 @@
+int findMin(int* nums, int numsSize) {
+    printf("Enter size of an array : ");
+    scanf("%d",&n);
+    int* nums[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&numns[i]);
+        if(nums[i]<nums[i+1]){
+            nums[i] = nums[i+1];
+        }
+    }
+}
